1c06b751b7189fe1edbdc4d91962c5ffa013c25b
yzx <yangzx53@mail2.sysu.edu.cn>
2024-12-02 18:42:58 +0800

    Initial commit

---
+++ 4.Experimental Results Online Estimation of Static Payloads/Case3.1/ReadMe.m
@@ -0,0 +1,2 @@
+% f_bias represents the weighted mean value of UDE corresponding to mL = 0g as shown in Table VII in this paper.
+% f_bias = 0.0096 in estimation experiments of static payloads at 0 rad.
---
+++ 5.Experimental Results Online Estimation of Dynamic Payloads/Case2.1/ReadMe.m
@@ -0,0 +1,2 @@
+% f_bias represents the weighted mean value of UDE corresponding to mL = 0g as shown in Table VII in this paper.
+% f_bias = - 0.0982 in estimation experiments of dynamic payloads at 0.2 rad.
